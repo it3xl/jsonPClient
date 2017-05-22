fa6ade4e7f9bfbd946c4f30c7d6a650e449c1d0b
Ilya Tretyakov <it@it3xl.ru>
2017-05-22 13:32:22 +0300

    Initial commit.

---
+++ jsonp_client.js
@@ -0,0 +1,36 @@
+function jsonPClient(urlTarget, success, error) {
+    var stamp = new Date().valueOf();
+    var callbackName = 'jsonPClient_' + Math.round(10000 * Math.random());
+    window[callbackName] = function(data){
+        cleanUp();
+        success(data);
+    }
+
+    var src = urlTarget + '?callback=' + callbackName;
+
+    var scriptTarget = document.createElement('script');
+    scriptTarget.async = "async";
+    //scriptTarget.charset = ...
+    scriptTarget.src = src;
+
+    scriptTarget.onerror = function(a, b, c){
+        cleanUp();
+        error(a, b, c);
+    };
+
+    var scriptHost = getScriptHost();
+    scriptHost.insertBefore(scriptTarget, scriptHost.firstChild );
+
+    function cleanUp(){
+        delete window[callbackName];
+
+        scriptHost.removeChild(scriptTarget);
+        scriptHost = null;
+        scriptTarget.onerror = null;
+        scriptTarget = null;
+    }
+    function getScriptHost(){
+        var scriptHost = document.head || document.getElementsByTagName("head")[0] || document.documentElement;
+        return scriptHost;
+    };
+};
